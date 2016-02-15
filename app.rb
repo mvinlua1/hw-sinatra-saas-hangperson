ac0d25350db7022083bf8bcf31127495eb62b88c
mvinlua1 <mvinlua1@binghamton.edu>
2016-02-15 16:02:40 +0000

    Through repeated guesses coverage

--- app.rb
+++ app.rb
@@ -40,6 +40,14 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
+    begin
+      if !@game.guess letter
+        flash[:message] = "You have already used that letter."
+      end
+    rescue ArgumentError
+      flash[:message] = "Invalid guess."
+      redirect '/show'
+    end
     redirect '/show'
   end
   
@@ -50,6 +58,10 @@ class HangpersonApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
+    state = @game.check_win_or_lose
+    if state == :win then redirect '/win'
+    elsif state == :lose then redirect '/lose'
+    end
     erb :show # You may change/remove this line
   end
   
